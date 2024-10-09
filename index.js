acde17ea0582570e382ff76e9388fa3fd1533dc9
otiliacojocaru <otilicacojocaru0708@gmail.com>
2024-10-09 21:04:48 +0300

    lectia12

---
+++ index.js
@@ -0,0 +1,145 @@
+// var let const
+
+// var x = 1;
+
+// let y = 2;
+
+// const z = 3;
+
+// x = 'new'
+
+// console.log(x);
+
+
+// tipuri de date primitive:
+// const num = 1;
+// const str = 'string';
+// const bool = true;
+// const undief = undefined;
+// const nul = null;
+
+// const symb = symbol;
+// const bigint = bigint(11);
+
+
+// tipuri de date nonprimitive:
+
+// const obj1 = {
+//     a: 4,
+//     b: 5,
+//     c: 6
+// }
+
+// const obj2 = {
+//     a: 4,
+//     b: 5,
+//     c: 6
+// }
+
+// const obj2 = obj1
+
+// obj2.a = 99;
+
+// console.log('obj1', obj1)
+// console.log('obj2', obj2)
+ 
+
+// let num1 = 1;
+// let num2 = num1;
+// num1 = 4
+
+// console.log('num1', num1)
+// console.log('num2', num2)
+
+// const num1 = 3;
+// const num2 = 4;
+
+// console.log(num1 + num2)
+
+// let str1 = 'string1';
+// let str2 = 3;
+
+// console.log(num1 + str2);
+
+
+// console.log(num2 - num1)
+
+// console.log(typeof 3)
+
+// const num1 = 3;
+// console.log('type of num1 is:', typeof num1)
+
+// const str = 'string';
+// console.log('type of str is:', typeof str)
+
+// const hool = true;
+// console.log('type of hool is:', typeof hool)
+
+// const undief = undefined;
+// console.log('type of undief is:', typeof undief)
+
+// const nul = null
+// console.log('type of nul is:', typeof nul)
+
+// const bigInt = BigInt(33)
+// console.log('type of bigint is:', typeof bigInt)
+
+// const symb = Symbol('2')
+// console.log('type of symb is:', typeof symb)
+
+// const obj = {};
+// console.log('type of obj is:', typeof obj)
+
+
+// console.log('adunare', 3 + 3)
+// console.log('scadere', 2 - 3)
+// console.log('adunare', 'string' + 'string2')
+
+
+// console.log(0 == false)
+// console.log(0 === false)
+
+//(== equal)
+//(=== strong equal)
+
+// console.log(3 == '3')
+// console.log(3 === '3')
+
+// console.log(0==false)
+// console.log(0===false)
+
+//console.log(3=='5')
+
+// const folsy1 = 0;
+// const folsy2 =''
+// const folsy3 = undefined;
+// const folsy4 = null;
+// const folsy5 = NaN;
+
+// console.log(!Boolean(folsy1))  //! este negarea
+// console.log(Boolean(folsy2))
+// console.log(Boolean(folsy3))
+// console.log(Boolean(folsy4))
+// console.log(Boolean(folsy5))
+// console.log(!!folsy1)  //!!dublu negarea
+
+
+// console.log(3*5)
+// console.log(3/4)
+
+// let num1 =5
+
+// num1 = num1 + 1
+// num1 = 5
+// num1++  //++ adauga 1
+
+// console.log(num1)
+// console.log(num1++)
+// console.log(num1)   //randul 132 si 133 este egal cu 136 si 137
+
+
+// let decr = 6
+
+// console.log(--decr)
+
+console.log(3**3) //ridicarea la putere **
